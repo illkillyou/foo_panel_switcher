185d4ee644fce8eec6740b314f34f34b2ee3f7d1
illkillyou <therobloxsnoopy@gmail.com>
2026-08-16 02:26:14 -0400

    Updated about

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 DECLARE_COMPONENT_VERSION(
     "Panel Switcher",
     "1.1",
-    "Container for switching between foobar2000 UI elements."
+    "foo_panel_switcher v1.1\nContainer for switching between foobar2000 UI elements.\nSource + More Info @: https://github.com/illkillyou/foo_panel_switcher"
 );
 
 VALIDATE_COMPONENT_FILENAME("foo_panel_switcher.dll");
